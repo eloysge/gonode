405d6d97b2800125ffca2b81ff9e7378319ed104
Eloy Gontijo Martins <eloy@sgeinformatica.com.br>
2020-01-07 11:01:37 -0300

    fix use youch

--- app/Models/File.js
+++ app/Models/File.js
@@ -5,11 +5,15 @@ const Env = use('Env')
 
 class File extends Model {
   static get computed () {
-    return ['url']
+    return ['url', 'Token']
   }
 
   getUrl ({ id }) {
     return `${Env.get('APP_URL')}/files/${id}`
+  }
+
+  getToken ({ id, name }) {
+    return `${id}/token/${name}`
   }
 }
 
